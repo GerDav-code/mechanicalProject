89f395b0f6644e9e3531a3470ce8a2c5c0c52825
GerDav-code <gerardodaviddamianalonso@gmail.com>
2026-08-13 09:01:35 -0600

    feat(rescues): crear estructura base para rescates, tracking y notificaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RescuesModule } from './modules/rescues/rescues.module';
+import { TrackingModule } from './modules/tracking/tracking.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [RescuesModule, TrackingModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
