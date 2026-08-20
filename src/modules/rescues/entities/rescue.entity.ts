0646189d350203aad22ec5f05424dd486e4134a0
GerDav-code <gerardodaviddamianalonso@gmail.com>
2026-08-19 23:20:40 -0600

    feat(rescues): configurar entidad Rescue

--- src/modules/rescues/entities/rescue.entity.ts
+++ src/modules/rescues/entities/rescue.entity.ts
@@ -1,14 +1,39 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 import { RescueStatus } from '../enums/rescue-status.enum';
 
+@Entity('rescues') 
 export class Rescue {
+  @PrimaryGeneratedColumn('uuid') 
   id!: string;
+
+  @Column()
   clientId!: string;
-  mechanicId?: string; // Opcional (lleva signo ? porque al inicio no hay mecánico)
+
+  @Column({ nullable: true }) 
+  mechanicId?: string;
+
+  @Column()
   vehicleId!: string;
+
+  @Column({ type: 'float' })
   latitude!: number;
+
+  @Column({ type: 'float' })
   longitude!: number;
+
+  @Column({ type: 'text' })
   description!: string;
+
+  @Column({
+    type: 'enum',
+    enum: RescueStatus,
+    default: RescueStatus.PENDING,
+  })
   status!: RescueStatus;
+
+  @CreateDateColumn()
   createdAt!: Date;
+
+  @UpdateDateColumn()
   updatedAt!: Date;
 }
